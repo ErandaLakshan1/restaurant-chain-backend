28d20b256add1a68583bc19f3dd3ca9a59a7faee
ErandaLakshan1 <eranda610@gmail.com>
2024-08-14 23:13:37 +0530

    creating the update and delete branches

--- branches/urls.py
+++ branches/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('get_branches/', views.get_branches),
     path('get_branches/<int:pk>/', views.get_branches),
+
     path('add_branch/', views.create_branch),
+    path('update_branch/<int:pk>/', views.update_branch),
+
+    path('delete_branch/<int:pk>/', views.delete_branch),
 ]
--- branches/views.py
+++ branches/views.py
@@ -12,7 +12,6 @@ from . import serializers
 @api_view(['GET'])
 def get_branches(request, pk=None, *args, **kwargs):
     if pk:
-        
         try:
             # Retrieve the specific branch
             branch = models.Branch.objects.get(pk=pk)
@@ -44,3 +43,48 @@ def create_branch(request, *args, **kwargs):
         serializer.save()
         return Response({"detail": "Branch created successfully."}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# to update branch
+@api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
+def update_branch(request, pk, *args, **kwargs):
+    user = request.user
+    user_type = getattr(user, 'user_type')
+
+    if user_type != 'admin':
+        return Response({"detail": "You do not have permission to perform this action."},
+                        status=status.HTTP_403_FORBIDDEN)
+
+    try:
+        branch_data = models.Branch.objects.get(pk=pk)
+        serializer = serializers.BranchSerializer(branch_data, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"detail": "Branch updated successfully."}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    except models.Branch.DoesNotExist:
+        return Response({"detail": "Branch not found."}, status=status.HTTP_404_NOT_FOUND)
+
+
+# to delete branch
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_branch(request, pk, *args, **kwargs):
+    user = request.user
+    user_type = getattr(user, 'user_type')
+
+    if user_type != 'admin':
+        return Response({"detail": "You do not have permission to perform this action."},
+                        status=status.HTTP_403_FORBIDDEN)
+
+    try:
+        branch_data = models.Branch.objects.get(pk=pk)
+        branch_data.delete()
+        return Response({"detail": "Branch deleted successfully."}, status=status.HTTP_200_OK)
+
+    except models.Branch.DoesNotExist:
+        return Response({"detail": "Branch not found."}, status=status.HTTP_404_NOT_FOUND)
