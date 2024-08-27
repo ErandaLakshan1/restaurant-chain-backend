c5200dd00def7361715cb63ca0a1b002c46c187c
ErandaLakshan1 <eranda610@gmail.com>
2024-08-27 18:05:37 +0530

    creating api for add and delete images according to menu item

--- menu/urls.py
+++ menu/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('get/menu_item/<int:pk>/', views.get_menu_items),
     path('get/menu_item_by_user/<int:branch_id>/<int:pk>/', views.get_meu_items_according_to_branch),
     path('get/menu_items_by_user/<int:branch_id>/', views.get_meu_items_according_to_branch),
+
+    path('update/menu_item/<int:pk>/', views.update_menu_item),
+    path('update/menu_item_images/<int:pk>/', views.add_images_to_menu_item),
+
+    path('delete/menu_item_images/<int:pk>/', views.delete_menu_item_images),
+
+
 ]
--- menu/views.py
+++ menu/views.py
@@ -127,3 +127,112 @@ def get_meu_items_according_to_branch(request, branch_id, pk=None, *args, **kwar
     menu_items = models.Menu.objects.filter(branch=branch_id)
     serializer = serializers.MenuSerializer(menu_items, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# to update menu items
+@api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
+def update_menu_item(request, pk, *args, **kwargs):
+    user = request.user
+    user_type = getattr(user, 'user_type')
+    user_branch = getattr(user, 'branch')
+
+    if user_type in ['customer', 'delivery_partner']:
+        return Response({"detail: You do not have permission to perform this actions."},
+                        status=status.HTTP_403_FORBIDDEN)
+
+    try:
+        menu_item = models.Menu.objects.get(pk=pk)
+
+    except models.Menu.DoesNotExist:
+        return Response({"detail": "Menu item does not exist."}, status=status.HTTP_404_NOT_FOUND)
+
+    if user_type in ['manager', 'staff']:
+        if menu_item.branch != user_branch:
+            return Response({"detail": "You do not have permission to update this menu item."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+    serializer = serializers.MenuSerializer(menu_item, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"detail": "Menu item updated successfully."}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# to delete menu item images
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_menu_item_images(request, pk, *args, **kwargs):
+    user = request.user
+    user_type = getattr(user, 'user_type')
+    user_branch = getattr(user, 'branch')
+
+    if user_type in ['customer', 'delivery_partner']:
+        return Response({"detail: You do not have permission to perform this actions."},
+                        status=status.HTTP_403_FORBIDDEN)
+
+    try:
+        menu_item_image = models.MenuImage.objects.get(pk=pk)
+    except models.MenuImage.DoesNotExist:
+        return Response({"detail": "Menu item image does not exist."}, status=status.HTTP_404_NOT_FOUND)
+
+    menu_item = menu_item_image.menu
+
+    if user_type in ['manager', 'staff'] and menu_item.branch != user_branch:
+        return Response({"detail": "You do not have permission to perform this action."}, status=status.HTTP_403_FORBIDDEN)
+
+    # extract the file name from the image url
+    image_url = menu_item_image.image_url
+    file_name = image_url.split('/')[-1]
+
+    # delete the image form firebase
+    blob = bucket.blob(f'menu_images/{file_name}')
+    if blob.exists():
+        blob.delete()
+
+    # delete the branch image form the database
+    menu_item_image.delete()
+    return Response({"detail": "Menu item image deleted successfully."}, status=status.HTTP_200_OK)
+
+
+# to add images to menu items
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_images_to_menu_item(request, pk, *args, **kwargs):
+    user = request.user
+    user_type = getattr(user, 'user_type')
+    user_branch = getattr(user, 'branch')
+
+    if user_type in ['customer', 'delivery_partner']:
+        return Response({"detail: You do not have permission to perform this actions."},
+                        status=status.HTTP_403_FORBIDDEN)
+
+    try:
+        menu_item = models.Menu.objects.get(pk=pk)
+    except models.Menu.DoesNotExist:
+        return Response({"detail": "Menu item does not exist."}, status=status.HTTP_404_NOT_FOUND)
+
+    if user_type in ['manager', 'staff'] and menu_item.branch != user_branch:
+        return Response({"detail": "You do not have permission to perform this action."}, status=status.HTTP_403_FORBIDDEN)
+
+    image_files = request.FILES.getlist('images')
+
+    if not image_files:
+        return Response({"detail": "No images provided."}, status=status.HTTP_400_BAD_REQUEST)
+
+    menu_item_images = []
+
+    for image in image_files:
+        if not isinstance(image, InMemoryUploadedFile):
+            return Response({"detail": "Uploaded file is not in the correct format."}, status=status.HTTP_400_BAD_REQUEST)
+
+        blob = bucket.blob(f'menu_images/{image.name}')
+        blob.upload_from_file(image, content_type=image.content_type)
+        blob.make_public()
+        image_url = blob.public_url
+
+        menu_item_image = models.MenuImage(menu=menu_item, image_url=image_url)
+        menu_item_images.append(menu_item_image)
+
+    models.MenuImage.objects.bulk_create(menu_item_images)
+    return Response({"detail": "Images added successfully."}, status=status.HTTP_200_OK)
