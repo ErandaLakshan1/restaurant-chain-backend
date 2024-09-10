86f07bdd77c5cb486fc750e7c194855bdcb5635d
ErandaLakshan1 <eranda610@gmail.com>
2024-09-10 11:51:42 +0530

    make changes in gitignore file

--- firebase_config.py
+++ firebase_config.py
@@ -1,16 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, storage
 import os
+import json
 
 from dotenv import load_dotenv
 # from data import firebase_bucket_url
 load_dotenv()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-cred_path = os.path.join(BASE_DIR, 'firebase_cred.json')
-
+cred_dict = json.loads(os.getenv('FIREBASE_CREDENTIALS_JSON'))
 # initialize the Firebase Admin SDK
-cred = credentials.Certificate(cred_path)
+cred = credentials.Certificate(cred_dict)
 # initialize the Firebase Admin SDK
 firebase_admin.initialize_app(cred, {
     'storageBucket': os.getenv('FIREBASE_BUCKET_URL')
